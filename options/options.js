d173d61c5e47546bf4f914a4325333752496e71b
Crowdbotics <team@crowdbotics.com>
2021-09-05 17:45:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_credit_30330Identifier",
+  url: "https://aged_credit_30330Identifier.botics.co",
+  api: "https://aged_credit_30330Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
